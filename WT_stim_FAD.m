f5adf6640aa157f22c4e720c841f8f33b843f786
xwang167 <xiaodanwang@wustl.edu>
2022-05-29 16:24:38 -0500

    update

---
+++ WT_stim_FAD.m
@@ -0,0 +1,36 @@
+excelFile = 'X:\Paper\PaperExperiment.xlsx';
+excelRows = 12:16;
+runs = 1:3;
+
+for excelRow = excelRows
+    [~, ~, excelRaw]=xlsread(excelFile,1, ['A',num2str(excelRow),':V',num2str(excelRow)]);
+    recDate = excelRaw{1}; recDate = string(recDate);
+    mouseName = excelRaw{2}; mouseName = string(mouseName);
+    rawdataloc = excelRaw{3};
+    saveDir = excelRaw{4}; saveDir = fullfile(string(saveDir),recDate);
+    sessionType = excelRaw{6}; sessionType = sessionType(3:end-2);
+    if ~exist(saveDir)
+        mkdir(saveDir)
+    end
+    sessionInfo.mouseType = excelRaw{17};
+    sessionInfo.darkFrameNum = excelRaw{15};
+    systemType = excelRaw{5};
+    sessionInfo.framerate = excelRaw{7};
+        saveDir_new = strcat('L:\RGECO\Kenny\', recDate, '\');
+    maskName = strcat(recDate,'-',mouseName,'-',sessionType,'1-datafluor','.mat');
+    
+    if ~exist(fullfile(saveDir_new,maskName),'file')
+        maskName = strcat(recDate,'-',mouseName,'-LandmarksAndMask','.mat');
+        load(fullfile(saveDir,maskName),'xform_isbrain')
+    else
+        load(fullfile(saveDir_new,maskName),'xform_isbrain')
+    end
+
+    for n = runs
+        processedName = strcat(recDate,'-',mouseName,'-',sessionType,num2str(n),'_processed','.mat');
+        load(fullfile(saveDir,processedName),'xform_FADCorr')
+        xform_FADCorr_GSR = gsr(xform_FADCorr,xform_isbrain);
+        figure(1)
+        
+    end
+end
