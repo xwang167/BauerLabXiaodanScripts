c7547816b3bcc596c46786b441f66d4b32df8086
xwang167 <xiaodanwang@wustl.edu>
2022-12-16 14:18:36 -0600

    Deconvolution

--- Deconvolution_CalciumFAD_awake_NoGSR.m
+++ Deconvolution_CalciumFAD_awake_NoGSR.m
@@ -1,8 +1,8 @@
 clear ;close all;clc
-excelFile = "C:\Users\xiaodanwang\Documents\GitHub\BauerLabXiaodanScripts\DataBase_Xiaodan.xlsx";
-startInd = 1;
-freqLow = 0.01;
-freqHigh = 5;
+excelFile = "X:\RGECO\DataBase_Xiaodan_3.xlsx";
+startInd = 2;
+freqLow = 0.02;
+freqHigh = 2;
 calMax = 8;
 FADMax = 1;
 mrfMax = 0.05;
@@ -15,8 +15,7 @@ t = (-3*freq:(t_kernel-3)*freq-1)/freq;
 load('AtlasandIsbrain.mat','AtlasSeeds')
 mask_barrel = AtlasSeeds==9;
 
-
-for excelRow = [181 183 185 228 232 236]
+for excelRow = [236]%181 183 185 228 232 
     
     [~, ~, excelRaw]=xlsread(excelFile,1, ['A',num2str(excelRow),':V',num2str(excelRow)]);
     recDate = excelRaw{1}; recDate = string(recDate);
@@ -27,19 +26,21 @@ for excelRow = [181 183 185 228 232 236]
         mkdir(strcat(saveDir,'\Barrel_mrf'))
     end
     for n = 1:3
+        tic 
+        disp(strcat(mouseName,', run#',num2str(n)))
         processedName = strcat(recDate,'-',mouseName,'-',sessionType,num2str(n),'_processed','.mat');
         %load contrasts
-        load(fullfile(saveDir,processedName),'xform_FADCorr','xform_jrgeco1aCorr')
-        maskDir = strcat('L:\RGECO\Kenny\', recDate, '\');
-        %load xform_isbrain
-        if exist(fullfile(maskDir,strcat(recDate,'-',mouseName,'-',sessionType,num2str(1),'-dataFluor.mat')),'file')
-            load(fullfile(maskDir,strcat(recDate,'-',mouseName,'-',sessionType,num2str(1),'-dataFluor.mat')),'xform_isbrain');
-            
-        else
-            maskDir = saveDir;
-            maskName = strcat(recDate,'-',mouseName,'-LandmarksAndMask','.mat');
-            load(fullfile(maskDir,maskName),'affineMarkers','xform_isbrain')
-        end
+        load(fullfile(saveDir,processedName),'xform_FADCorr','xform_jrgeco1aCorr','xform_isbrain')
+%         maskDir = strcat('L:\RGECO\Kenny\', recDate, '\');
+%         %load xform_isbrain
+%         if exist(fullfile(maskDir,strcat(recDate,'-',mouseName,'-',sessionType,num2str(1),'-dataFluor.mat')),'file')
+%             load(fullfile(maskDir,strcat(recDate,'-',mouseName,'-',sessionType,num2str(1),'-dataFluor.mat')),'xform_isbrain');
+%             
+%         else
+%             maskDir = saveDir;
+%             maskName = strcat(recDate,'-',mouseName,'-LandmarksAndMask','.mat');
+%             load(fullfile(maskDir,maskName),'affineMarkers','xform_isbrain')
+%         end
         FAD = squeeze(xform_FADCorr)*100;% convert to DeltaF/F%
         clear xform_FADCorr
         FAD(:,:,end+1) = FAD(:,:,end);
@@ -55,15 +56,16 @@ for excelRow = [181 183 185 228 232 236]
         Calcium = resample(Calcium,freq,samplingRate,'Dimension',3); %resample to 250 Hz
         
         % filter again
+        tic
         FAD =  filterData(FAD,freqLow,freqHigh,freq);
         Calcium = filterData(Calcium,freqLow,freqHigh,freq);
-        
+        toc
         % Reshape into 30 seconds
         FAD=reshape(FAD,128,128,t_kernel*freq,[]);
         Calcium=reshape(Calcium,128,128,t_kernel*freq,[]);
         
-        r_Barrel_mrf = zeros(1,11-startInd);
-        mrf_Barrel = zeros(11-startInd,length(Calcium));
+        r_Barrel_mrf_0p02_2 = zeros(1,21-startInd);
+        mrf_Barrel_0p02_2 = zeros(21-startInd,length(Calcium));
         jj = 1;
         for ii = startInd:20
             
@@ -84,15 +86,15 @@ for excelRow = [181 183 185 228 232 236]
             %X = X(151:450,:);
             X = X(1:length(Calcium_barrel),1:length(Calcium_barrel));% make it square?
             [~,S,~]=svd(X);
-            lambda = 0.01;
-            h_region_barrel = (X'*S*X+(S(1,1).^3)*lambda*eye(length(Calcium_barrel))) \ (X'*S*[zeros(3*freq,1); FAD_barrel(1:end-3*freq)]);% why add 3s of zeros? Do we need to shift it?
-            mrf_Barrel(jj,:) = h_region_barrel;
+            lambda = 0.0005;
+            h_region_barrel = (X'*S*X+(S(1,1).^1)*lambda*eye(length(Calcium_barrel))) \ (X'*S*[zeros(3*freq,1); FAD_barrel(1:end-3*freq)]);% why add 3s of zeros? Do we need to shift it?
+            mrf_Barrel_0p02_2(jj,:) = h_region_barrel;
             
             % Predicted FAD
             FAD_barrel_pred = conv(Calcium_barrel,h_region_barrel);
             FAD_barrel_pred = FAD_barrel_pred(1:(length(FAD_barrel)+3*freq));
             r_region_barrel = corr(FAD_barrel,FAD_barrel_pred(3*freq+1:end));
-            r_Barrel_mrf(jj) = r_region_barrel;
+            r_Barrel_mrf_0p02_2(jj) = r_region_barrel;
             
             jj = jj+1;
             figure('units','normalized','outerposition',[0 0 1 1])
@@ -112,36 +114,46 @@ for excelRow = [181 183 185 228 232 236]
             subplot(2,2,2)
             plot(t,h_region_barrel)
             xlim([-3 10])
-            %ylim([-mrfMax mrfMax])
-            ylabel('\Delta\muM/\DeltaF/F%')
+        
             xlabel('Time(s)')
             title('mrf for Barrel Cortex')
 
+            test = abs(FAD_barrel);
+            maxVal = max(test);
             subplot(2,2,3)
             yyaxis left
             plot((1:t_kernel*freq)/freq,FAD_barrel,'g')
+            ylim([-maxVal maxVal])
             hold on
+            test = abs(FAD_barrel_pred);
+            maxVal = max(test);
             yyaxis right
             plot((1:t_kernel*freq)/freq,FAD_barrel_pred(3*freq+1:3*freq+length(FAD_barrel)),'k')
+            ylim([-maxVal maxVal])
             xlabel('Time(s)')
             ylabel('\DeltaF/F%')
             legend('Actual FAD','Predicted FAD')
             title(strcat('r = ',num2str(r_region_barrel)))
-            
-            sgtitle(strcat('mrf for Barrel Region, ',num2str(freqLow),'-',freqHigh,', ',' lambda = ',num2str(lambda),', ',mouseName,' Run #',num2str(n),', Segment #',num2str(ii)))
+            sgtitle(strcat('mrf for Barrel Region, ',num2str(freqLow),'-',num2str(freqHigh),', ',' lambda = ',num2str(lambda),', ',mouseName,' Run #',num2str(n),', Segment #',num2str(ii)))
             saveName =  fullfile(saveDir,'Barrel_mrf', strcat(recDate,'-',mouseName,'-',sessionType,num2str(n),'-segment#',num2str(ii),'-Barrel-NoGSR-mrf'));
             saveas(gcf,strcat(saveName,'.fig'))
             saveas(gcf,strcat(saveName,'.png'))
             close all
         end
         
-        save(fullfile(saveDir,'Barrel_mrf', strcat(recDate,'-',mouseName,'-',sessionType,num2str(n),'_Barrel_mrf','.mat')),'r_Barrel_mrf','mrf_Barrel')
+          clear FAD Calcium
+        if exist(fullfile(saveDir,'Barrel_mrf', strcat(recDate,'-',mouseName,'-',sessionType,num2str(n),'_Barrel_mrf','.mat')),'file')
+            save(fullfile(saveDir,'Barrel_mrf', strcat(recDate,'-',mouseName,'-',sessionType,num2str(n),'_Barrel_mrf','.mat')),'r_Barrel_mrf_0p02_2','mrf_Barrel_0p02_2','-append')
+        else
+            save(fullfile(saveDir,'Barrel_mrf', strcat(recDate,'-',mouseName,'-',sessionType,num2str(n),'_Barrel_mrf','.mat')),'r_Barrel_mrf_0p02_2','mrf_Barrel_0p02_2')
+        end
+        toc 
     end
 end
 
 %% Averaged over r>0.6 only
 totalNum = 0;
-mrf_Barrel_0p6 = [];
+mrf_Barrel_0p02_2_0p6 = [];
 miceName = [];
 for excelRow = [181 183 185 228 232 236]
     
@@ -155,18 +167,18 @@ for excelRow = [181 183 185 228 232 236]
         mkdir(strcat(saveDir,'\Barrel_mrf'))
     end
     for n = 1:3
-        load(fullfile(saveDir,'Barrel_mrf', strcat(recDate,'-',mouseName,'-',sessionType,num2str(n),'_Barrel_mrf','.mat')),'r_Barrel_mrf','mrf_Barrel')
-        totalNum = totalNum + length(r_Barrel_mrf);
-        mrf_Barrel_0p6 = cat(1,mrf_Barrel_0p6,mrf_Barrel(r_Barrel_mrf>0.6,:));
+        load(fullfile(saveDir,'Barrel_mrf', strcat(recDate,'-',mouseName,'-',sessionType,num2str(n),'_Barrel_mrf','.mat')),'r_Barrel_mrf_0p02_2','mrf_Barrel_0p02_2')
+        totalNum = totalNum + length(r_Barrel_mrf_0p02_2);
+        mrf_Barrel_0p02_2_0p6 = cat(1,mrf_Barrel_0p02_2_0p6,mrf_Barrel_0p02_2(r_Barrel_mrf_0p02_2>0.6,:));
     end
 end
-qualifiedNum = size(mrf_Barrel_0p6,1);
-mrf_Barrel_0p6_median = median(mrf_Barrel_0p6);
-save(strcat('L:\RGECO\cat\Barrel_mrf\',recDate,miceName,'-Barrel_mrf.mat'),'mrf_Barrel_0p6','qualifiedNum','totalNum');
+qualifiedNum = size(mrf_Barrel_0p02_2_0p6,1);
+mrf_Barrel_0p02_2_0p6_median = median(mrf_Barrel_0p02_2_0p6);
+save(strcat('L:\RGECO\cat\Barrel_mrf\',recDate,miceName,'-Barrel_mrf.mat'),'mrf_Barrel_0p02_2_0p6','qualifiedNum','totalNum');
 
-[pks,locs,w,p] = findpeaks(mrf_Barrel_0p6_median,t,'MinPeakProminence',2e-9);
+[pks,locs,w,p] = findpeaks(mrf_Barrel_0p02_2_0p6_median,t,'MinPeakProminence',2e-9);
 figure
-plot_distribution_prctile(t,mrf_Barrel_0p6)
+plot_distribution_prctile(t,mrf_Barrel_0p02_2_0p6)
 legend('25%-75%')
 xlim([-3 10])
 xlabel('Time(s)')
@@ -176,7 +188,8 @@ title(strcat(num2str(qualifiedNum),'/',num2str(totalNum),' has r>0.6, Median: T=
 % Averaged across all 
 totalNum = 0;
 mrf_Barrel_mice = [];
-r_Barrel_mrf_mice = [];
+mrf_Barrel_0p02_2_mice = [];
+r_Barrel_mrf_0p02_2_mice= [];
 miceName = [];
 for excelRow = [181 183 185 228 232 236]
     
@@ -187,24 +200,24 @@ for excelRow = [181 183 185 228 232 236]
     saveDir = excelRaw{4}; saveDir = fullfile(string(saveDir),recDate);
     sessionType = excelRaw{6}; sessionType = sessionType(3:end-2);
 
-    for n = 1:3
-        load(fullfile(saveDir,'Barrel_mrf', strcat(recDate,'-',mouseName,'-',sessionType,num2str(n),'_Barrel_mrf','.mat')),'mrf_Barrel','r_Barrel_mrf')
-        totalNum = totalNum + length(mrf_Barrel);
-        mrf_Barrel_mice = cat(1,mrf_Barrel_mice,mrf_Barrel);
-        r_Barrel_mrf_mice = [r_Barrel_mrf_mice,r_Barrel_mrf];
+    for n = 1:3;
+        load(fullfile(saveDir,'Barrel_mrf', strcat(recDate,'-',mouseName,'-',sessionType,num2str(n),'_Barrel_mrf','.mat')),'mrf_Barrel_0p02_2','r_Barrel_mrf_0p02_2')
+        totalNum = totalNum + length(mrf_Barrel_0p02_2);
+        mrf_Barrel_0p02_2_mice = cat(1,mrf_Barrel_0p02_2_mice,mrf_Barrel_0p02_2);
+        r_Barrel_mrf_0p02_2_mice = [r_Barrel_mrf_0p02_2_mice,r_Barrel_mrf_0p02_2];
     end
 end
-mrf_Barrel_mice_median = median(mrf_Barrel_mice);
-r_Barrel_mice_median = median(r_Barrel_mrf_mice);
-save(strcat('L:\RGECO\cat\Barrel_mrf\',recDate,miceName,'-Barrel_mrf.mat'),'mrf_Barrel_mice','r_Barrel_mice','-append');
+mrf_Barrel_0p02_2_mice_median = median(mrf_Barrel_0p02_2_mice);
+r_Barrel_mrf_0p02_2_mice_median = median(r_Barrel_mrf_0p02_2_mice);
+save(strcat('L:\RGECO\cat\Barrel_mrf\',recDate,miceName,'-Barrel_mrf.mat'),'mrf_Barrel_0p02_2_mice','-append');
 
-[pks,locs,w,p] = findpeaks(mrf_Barrel_mice_median,t,'MinPeakProminence',2e-9);
+[pks,locs,w,p] = findpeaks(mrf_Barrel_0p02_2_mice_median,t,'MinPeakProminence',2e-9);
 figure
 plot_distribution_prctile(t,mrf_Barrel_mice)
+plot_distribution_prctile(t,mrf_Barrel_0p02_2_mice)
 legend('25%-75%')
 xlim([-3 10])
 xlabel('Time(s)')
 grid on
-title(strcat('Median: T=',num2str(locs),'s, W=',num2str(w),'s, A=',num2str(pks),', r =',num2str(r_Barrel_mice_median)))
-
+title(strcat('Median: T=',num2str(locs),'s, W=',num2str(w),'s, A=',num2str(pks)', 'r=',num2str(r_Barrel_mrf_0p02_2_mice_median,'%4.2f')))
 
