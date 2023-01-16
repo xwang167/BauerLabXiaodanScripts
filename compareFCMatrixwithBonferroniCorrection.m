39effa72a7a27cd8bbc23095d55149f3078f295d
xwang167 <xiaodanwang@wustl.edu>
2023-01-16 15:04:12 -0600

    statistical difference

--- compareFCMatrixwithBonferroniCorrection.m
+++ compareFCMatrixwithBonferroniCorrection.m
@@ -1,6 +1,10 @@
-function [h,p] = compareFCMatrixwithBonferroniCorrection(alpha,vararg)
-
+function [h,p] = compareFCMatrixwithBonferroniCorrection(alpha,fcMatrix1,varargin)
 % fcMatrix is in # of seeds * # of Seeds * Contrasts * mice
+% if varargin empty, fcMatrix1 needs to be the difference between 2 matrix
+
+if numel(varargin) > 0
+    fcMatrix2 = varargin;
+end
 nPy = size(fcMatrix1,1);
 nPx = size(fcMatrix1,2);
 
@@ -13,7 +17,11 @@ alpha = alpha/((1-nPy)^2/2);
 for contrast = 1:size(fcMatrix1,3)
     for y = 2:nPy
         for x = 1:nPx-1
-            [h(y,x,contrast),p(ii,jj,contrast)] = ttest2(fcMatrix1(y,x,contrast,:), fcMatrix2(y,x,contrst,:), alpha, 'both', 'unequal'); 
+            if numel(varargin) > 0
+                [h(y,x,contrast),p(ii,jj,contrast)] = ttest2(fcMatrix1(y,x,contrast,:), fcMatrix2(y,x,contrst,:), alpha, 'both', 'unequal');
+            else
+                [h(y,x,contrast),p(ii,jj,contrast)] = ttest(fcMatrix1(y,x,contrast,:), alpha, 'both', 'unequal');
+            end
         end
     end
 end
