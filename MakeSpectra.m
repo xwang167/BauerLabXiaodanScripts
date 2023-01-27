60f51915a3b47a977d37b2c3e8189c5e007cbfb8
xwang167 <xiaodanwang@wustl.edu>
2023-01-26 21:35:22 -0600

    make spectra

---
+++ MakeSpectra.m
@@ -0,0 +1,31 @@
+ChR2 = xlsread("C:\Users\Xiaodan Wang\Documents\GitHub\newpipeline\Functions_and_Wrappers\+bauerParams\ChR2 Spectrum.csv");
+
+fid = fopen("C:\Users\Xiaodan Wang\Documents\GitHub\newpipeline\Functions_and_Wrappers\+bauerParams\probeSpectra\gcamp6f_excitation.txt");
+gcamp = textscan(fid,'%f %f','headerlines',0);
+fclose(fid);
+
+fid = fopen("C:\Users\Xiaodan Wang\Documents\GitHub\newpipeline\Functions_and_Wrappers\+bauerParams\probeSpectra\jrgeco1a_excitation.txt");
+rgeco = textscan(fid,'%f %f','headerlines',0);
+fclose(fid);
+
+figure
+plot(ChR2(:,1),ChR2(:,2)/max(ChR2(:,2)),'b-')
+hold on
+plot(gcamp{1,1},gcamp{1,2}/max(gcamp{1,2}),'g-')
+xlim([400,600])
+ylim([0,1.1])
+xlabel('Wavelength(nm)')
+ylabel('Relative Intensity')
+legend('ChR2 Activation','GCaMP6f Excitation','location','southwest')
+
+figure
+plot(ChR2(:,1),ChR2(:,2)/max(ChR2(:,2)),'b-')
+hold on
+plot(gcamp{1,1},gcamp{1,2}/max(gcamp{1,2}),'g-')
+hold on
+plot(rgeco{1,1},rgeco{1,2}/max(rgeco{1,2}),'m-')
+xlim([400,600])
+ylim([0,1.1])
+xlabel('Wavelength(nm)')
+ylabel('Relative Intensity')
+legend('ChR2 Activation','GCaMP6f Excitation','jRGECO1a Excitation','location','southwest')
