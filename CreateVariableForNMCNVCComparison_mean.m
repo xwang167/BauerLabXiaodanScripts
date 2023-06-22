38b687038c8a7dd0872f1c290dc70ce84aee033e
xwang167 <xiaodanwang@wustl.edu>
2023-06-22 13:30:55 -0500

    Deconvolution

--- CreateVariableForNMCNVCComparison_mean.m
+++ CreateVariableForNMCNVCComparison_mean.m
@@ -2,8 +2,7 @@ saveName = "D:\XiaodanPaperData\cat\deconvolution_allRegions.mat";
 load(saveName, 'HRF_mice_awake_allRegions', 'r_HRF_mice_awake_allRegions', 'MRF_mice_awake_allRegions', 'r_MRF_mice_awake_allRegions', 'HRF_mice_anes_allRegions', 'r_HRF_mice_anes_allRegions', 'MRF_mice_anes_allRegions', 'r_MRF_mice_anes_allRegions')
 load(saveName, 'A_HRF_mice_awake_allRegions', 'T_HRF_mice_awake_allRegions', 'W_HRF_mice_awake_allRegions', 'A_MRF_mice_awake_allRegions', 'T_MRF_mice_awake_allRegions', 'W_MRF_mice_awake_allRegions', 'A_HRF_mice_anes_allRegions', 'T_HRF_mice_anes_allRegions', 'W_HRF_mice_anes_allRegions', 'A_MRF_mice_anes_allRegions', 'T_MRF_mice_anes_allRegions', 'W_MRF_mice_anes_allRegions')
 % Exclude T W A that has T bigger than 0.2 for NMC under awake condition
-
-numMice = eval(strcat('length(excelRows_',condition{1},');'));
+numMice = 6;
 for region = 1:50
     for mouseInd =1:numMice
         if T_MRF_mice_awake_allRegions(mouseInd,region)>0.2
@@ -27,7 +26,45 @@ for condition = {'awake','anes'}
     end
 end
 
+load("AtlasandIsbrain_Allen.mat",'parcelnames','AtlasSeeds')
 ii = 1; 
+for jj = [3,4,6:25,28,29,31:50]
+    label_region{ii} = parcelnames{jj};
+    ii = ii+1;
+end
+save(saveName,'label_region','-append')
+
+saveName = "D:\XiaodanPaperData\cat\deconvolution_allRegions.mat";
+load(saveName, 'HRF_mice_awake_allRegions', 'r_HRF_mice_awake_allRegions', 'MRF_mice_awake_allRegions', 'r_MRF_mice_awake_allRegions', 'HRF_mice_anes_allRegions', 'r_HRF_mice_anes_allRegions', 'MRF_mice_anes_allRegions', 'r_MRF_mice_anes_allRegions')
+load(saveName, 'A_HRF_mice_awake_allRegions', 'T_HRF_mice_awake_allRegions', 'W_HRF_mice_awake_allRegions', 'A_MRF_mice_awake_allRegions', 'T_MRF_mice_awake_allRegions', 'W_MRF_mice_awake_allRegions', 'A_HRF_mice_anes_allRegions', 'T_HRF_mice_anes_allRegions', 'W_HRF_mice_anes_allRegions', 'A_MRF_mice_anes_allRegions', 'T_MRF_mice_anes_allRegions', 'W_MRF_mice_anes_allRegions')
+% Exclude T W A that has T bigger than 0.2 for NMC under awake condition
+numMice = 6;
+for region = 1:50
+    for mouseInd =1:numMice
+        if T_MRF_mice_awake_allRegions(mouseInd,region)>0.2
+            T_MRF_mice_awake_allRegions(mouseInd,region) =NaN;
+            W_MRF_mice_awake_allRegions(mouseInd,region) =NaN;
+            A_MRF_mice_awake_allRegions(mouseInd,region) =NaN;
+            r_MRF_mice_awake_allRegions(mouseInd,region) =NaN;
+        end
+    end
+end
+
+
+for condition = {'awake','anes'}
+    for h = {'HRF','MRF'}
+        for var = {'T','W','A','r'}
+              eval(strcat(var{1},'_',h{1},'_mice_',condition{1},'_exclude = ',var{1},'_',h{1},'_mice_',condition{1},'_allRegions(:,[3,4,6:25,28,29,31:50]);'));
+              eval(strcat('save(',char(39),saveName,char(39),',',...
+                   char(39),var{1},'_',h{1},'_mice_',condition{1},'_exclude',char(39),',',...
+                   char(39),'-append',char(39),')'))
+        end
+    end
+end
+
+load("AtlasandIsbrain_Allen.mat",'parcelnames','AtlasSeeds')
+ii = 1; 
+mask_exlude = zeros(128,128);
 for jj = [3,4,6:25,28,29,31:50]
     label_region{ii} = parcelnames{jj};
     ii = ii+1;
@@ -47,53 +84,6 @@ T_MRF_mice_awake_exclude_mean = nanmean(T_MRF_mice_awake_exclude,2);
 T_HRF_mice_anes_exclude_mean  = nanmean(T_HRF_mice_anes_exclude,2);
 T_MRF_mice_anes_exclude_mean  = nanmean(T_MRF_mice_anes_exclude,2);
 
-% sort
-[T_HRF_mice_awake_exclude_mean_sort,I_T_awake] = sort(T_HRF_mice_awake_exclude_mean);
-T_MRF_mice_awake_exclude_mean_sort = T_MRF_mice_awake_exclude_mean(I_T_awake);
-label_region_T_awake = label_region(I_T_awake);
-r_T_awake_mean = corr(T_HRF_mice_awake_exclude_mean_sort,T_MRF_mice_awake_exclude_mean_sort);
-
-[T_HRF_mice_anes_exclude_mean_sort,I_T_anes] = sort(T_HRF_mice_anes_exclude_mean);
-T_MRF_mice_anes_exclude_mean_sort = T_MRF_mice_anes_exclude_mean(I_T_anes);
-label_region_T_anes = label_region(I_T_anes);
-r_T_anes_mean = corr(T_HRF_mice_anes_exclude_mean_sort,T_MRF_mice_anes_exclude_mean_sort);
-
-% scatter plot
-% Time to Peak
-figure('units','normalized','outerposition',[0 0 1 1])
-subplot(211)
-yyaxis left
-scatter(1:44,T_HRF_mice_awake_exclude_mean_sort,'filled')
-ylabel('NVC T(s)')
-hold on
-yyaxis right
-scatter(1:44,T_MRF_mice_awake_exclude_mean_sort,'filled','d')
-ylabel('NMC T(s)')
-xlabel('Region')
-xticks([1:44])
-xtickangle(0)
-xticklabels(num2str(I_T_awake))
-a = get(gca,'XTickLabel');
-set(gca,'XTickLabel',a,'FontName','Times','fontsize',18)
-title(['Awake, r = ',num2str(r_T_awake_mean)])
-subplot(212)
-yyaxis left
-scatter(1:44,T_HRF_mice_anes_exclude_mean_sort,'filled')
-ylabel('NVC T(s)')
-hold on
-yyaxis right
-scatter(1:44,T_MRF_mice_anes_exclude_mean_sort,'filled','d')
-ylabel('NMC T(s)')
-title(['Anesthetized, r = ',num2str(r_T_anes_mean)])
-xlabel('Region')
-xticks([1:44])
-xtickangle(0)
-xticklabels(num2str(I_T_anes))
-a = get(gca,'XTickLabel');
-set(gca,'XTickLabel',a,'FontName','Times','fontsize',18)
-
-sgtitle('Time to Peak Correlation between NVC and NMC')
-
 %% Width
 W_HRF_mice_awake_exclude = W_HRF_mice_awake_exclude';
 W_MRF_mice_awake_exclude = W_MRF_mice_awake_exclude';
@@ -106,52 +96,6 @@ W_MRF_mice_awake_exclude_mean = nanmean(W_MRF_mice_awake_exclude,2);
 
 W_HRF_mice_anes_exclude_mean  = nanmean(W_HRF_mice_anes_exclude,2);
 W_MRF_mice_anes_exclude_mean  = nanmean(W_MRF_mice_anes_exclude,2);
-
-% sort
-[W_HRF_mice_awake_exclude_mean_sort,I_W_awake] = sort(W_HRF_mice_awake_exclude_mean);
-W_MRF_mice_awake_exclude_mean_sort = W_MRF_mice_awake_exclude_mean(I_W_awake);
-label_region_W_awake = label_region(I_W_awake);
-r_W_awake_mean = corr(W_HRF_mice_awake_exclude_mean_sort,W_MRF_mice_awake_exclude_mean_sort);
-
-[W_HRF_mice_anes_exclude_mean_sort,I_W_anes] = sort(W_HRF_mice_anes_exclude_mean);
-W_MRF_mice_anes_exclude_mean_sort = W_MRF_mice_anes_exclude_mean(I_W_anes);
-label_region_W_anes = label_region(I_W_anes);
-r_W_anes_mean = corr(W_HRF_mice_anes_exclude_mean_sort,W_MRF_mice_anes_exclude_mean_sort);
-
-figure('units','normalized','outerposition',[0 0 1 1])
-subplot(211)
-yyaxis left
-scatter(1:44,W_HRF_mice_awake_exclude_mean_sort,'filled')
-ylabel('NVC W(s)')
-hold on
-yyaxis right
-scatter(1:44,W_MRF_mice_awake_exclude_mean_sort,'filled','d')
-ylabel('NMC W(s)')
-xlabel('Region')
-xticks([1:44])
-xtickangle(0)
-xticklabels(num2str(I_W_awake))
-a = get(gca,'XTickLabel');
-set(gca,'XTickLabel',a,'FontName','Times','fontsize',18)
-title(['Awake, r = ',num2str(r_W_awake_mean)])
-
-subplot(212)
-yyaxis left
-scatter(1:44,W_HRF_mice_anes_exclude_mean_sort,'filled')
-ylabel('NVC W(s)')
-hold on
-yyaxis right
-scatter(1:44,W_MRF_mice_anes_exclude_mean_sort,'filled','d')
-ylabel('NMC W(s)')
-title(['Anesthetized, r = ',num2str(r_W_anes_mean)])
-xlabel('Region')
-xticks([1:44])
-xtickangle(0)
-xticklabels(num2str(I_W_anes))
-a = get(gca,'XTickLabel');
-set(gca,'XTickLabel',a,'FontName','Times','fontsize',18)
-
-sgtitle('Width Correlation between NVC and NMC')
 
 %% Amplitude
 A_HRF_mice_awake_exclude = A_HRF_mice_awake_exclude';
@@ -166,107 +110,118 @@ A_MRF_mice_awake_exclude_mean = nanmean(A_MRF_mice_awake_exclude,2);
 A_HRF_mice_anes_exclude_mean  = nanmean(A_HRF_mice_anes_exclude,2);
 A_MRF_mice_anes_exclude_mean  = nanmean(A_MRF_mice_anes_exclude,2);
 
-% sort
-[A_HRF_mice_awake_exclude_mean_sort,I_A_awake] = sort(A_HRF_mice_awake_exclude_mean);
-A_MRF_mice_awake_exclude_mean_sort = A_MRF_mice_awake_exclude_mean(I_A_awake);
-label_region_A_awake = label_region(I_A_awake);
-r_A_awake_mean = corr(A_HRF_mice_awake_exclude_mean_sort,A_MRF_mice_awake_exclude_mean_sort);
-
-[A_HRF_mice_anes_exclude_mean_sort,I_A_anes] = sort(A_HRF_mice_anes_exclude_mean);
-A_MRF_mice_anes_exclude_mean_sort = A_MRF_mice_anes_exclude_mean(I_A_anes);
-label_region_A_anes = label_region(I_A_anes);
-r_A_anes_mean = corr(A_HRF_mice_anes_exclude_mean_sort,A_MRF_mice_anes_exclude_mean_sort);
-
+%% Visualization
 figure('units','normalized','outerposition',[0 0 1 1])
-subplot(211)
-yyaxis left
-scatter(1:44,A_HRF_mice_awake_exclude_mean_sort,'filled')
+
+% T
+subplot(321)
+scatter(T_MRF_mice_awake_exclude_mean,T_HRF_mice_awake_exclude_mean,[],linspace(1,10,44),'filled')
+mdl_T_awake = fitlm(T_MRF_mice_awake_exclude_mean,T_HRF_mice_awake_exclude_mean);
+hold on
+h = plot(mdl_T_awake) ;
+delete(h(1))
+xlabel('NMC T(s)')
+ylabel('NVC T(s)')
+colormap('inferno');
+title('T, Awake')
+legend off
+xlim([0.02 0.07])
+ylim([0.5 1.6])
+
+subplot(322)
+xlabel('NVC T(s)')
+scatter(T_MRF_mice_anes_exclude_mean,T_HRF_mice_anes_exclude_mean,[],linspace(1,10,44),'filled')
+mdl_T_anes = fitlm(T_MRF_mice_anes_exclude_mean,T_HRF_mice_anes_exclude_mean);
+hold on
+h = plot(mdl_T_anes) ;
+delete(h(1))
+xlabel('NMC T(s)')
+ylabel('NVC T(s)')
+colormap('inferno');
+title('T, Anes')
+legend off
+xlim([0.02 0.07])
+ylim([0.5 1.6])
+% W
+subplot(323)
+scatter(W_MRF_mice_awake_exclude_mean,W_HRF_mice_awake_exclude_mean,[],linspace(1,10,44),'filled')
+mdl_W_awake = fitlm(W_MRF_mice_awake_exclude_mean,W_HRF_mice_awake_exclude_mean);
+hold on
+h = plot(mdl_W_awake) ;
+delete(h(1))
+xlabel('NMC W(s)')
+ylabel('NVC W(s)')
+colormap('inferno');
+title('W, Awake')
+legend off
+xlim([0.3 0.65])
+ylim([0.8 2.7])
+
+subplot(324)
+scatter(W_MRF_mice_anes_exclude_mean,W_HRF_mice_anes_exclude_mean,[],linspace(1,10,44),'filled')
+mdl_W_anes = fitlm(W_MRF_mice_anes_exclude_mean,W_HRF_mice_anes_exclude_mean);
+hold on
+h = plot(mdl_W_anes) ;
+delete(h(1))
+xlabel('NMC W(s)')
+ylabel('NVC W(s)')
+colormap('inferno');
+title('W, Anes')
+legend off
+xlim([0.3 0.65])
+ylim([0.8 2.7])
+% A
+subplot(325)
+scatter(A_MRF_mice_awake_exclude_mean,A_HRF_mice_awake_exclude_mean,[],linspace(1,10,44),'filled')
+mdl_A_awake = fitlm(A_MRF_mice_awake_exclude_mean,A_HRF_mice_awake_exclude_mean);
+hold on
+h = plot(mdl_A_awake) ;
+delete(h(1))
+xlabel('NMC A')
 ylabel('NVC A')
-hold on
-yyaxis right
-scatter(1:44,A_MRF_mice_awake_exclude_mean_sort,'filled','d')
-ylabel('NMC A')
-xlabel('Region')
-xticks([1:44])
-xtickangle(0)
-xticklabels(num2str(I_A_awake))
-a = get(gca,'XTickLabel');
-set(gca,'XTickLabel',a,'FontName','Times','fontsize',18)
-title(['Awake, r = ',num2str(r_A_awake_mean)])
+colormap('inferno');
+title('A, Awake')
+legend off
+xlim([0.0005 0.0017])
+ylim([0.0004 0.005])
 
-subplot(212)
-yyaxis left
-scatter(1:44,A_HRF_mice_anes_exclude_mean_sort,'filled')
+subplot(326)
+scatter(A_MRF_mice_anes_exclude_mean,A_HRF_mice_anes_exclude_mean,[],linspace(1,10,44),'filled')
+mdl_A_anes = fitlm(A_MRF_mice_anes_exclude_mean,A_HRF_mice_anes_exclude_mean);
+hold on
+h = plot(mdl_A_anes) ;
+delete(h(1))
+xlabel('NMC A')
 ylabel('NVC A')
-hold on
-yyaxis right
-scatter(1:44,A_MRF_mice_anes_exclude_mean_sort,'filled','d')
-ylabel('NMC A')
-xlabel('Region')
-xticks([1:44])
-xtickangle(0)
-xticklabels(num2str(I_A_anes))
-a = get(gca,'XTickLabel');
-set(gca,'XTickLabel',a,'FontName','Times','fontsize',18)
-title(['Anesthetized, r = ',num2str(r_A_anes_mean)])
+colormap('inferno');
+title('A, Anes')
+legend off
+xlim([0.0005 0.0017])
+ylim([0.0004 0.005])
+%% Mask with color coded region
+load("AtlasandIsbrain_Allen.mat",'parcelnames','AtlasSeeds')
+excelFile = "X:\RGECO\DataBase_Xiaodan_3.xlsx";
+excelRows_awake = [181 183 185 228 232 236];
+excelRows_anes  = [202 195 204 230 234 240];
 
-sgtitle('Amplitude Correlation between NVC and NMC')
+xform_isbrain_mice = 1;
+for excelRow = [181 183 185 228 232 236 202 195 204 230 234 240]
+    [~, ~, excelRaw]=xlsread(excelFile,1, ['A',num2str(excelRow),':V',num2str(excelRow)]);
+    recDate = excelRaw{1}; recDate = string(recDate);
+    mouseName = excelRaw{2}; mouseName = string(mouseName);
+    saveDir = excelRaw{4}; saveDir = fullfile(string(saveDir),recDate);
+    sessionType = excelRaw{6}; sessionType = sessionType(3:end-2);
+    disp(strcat(mouseName,', run #1'))
+    processedName = strcat(recDate,'-',mouseName,'-',sessionType,'1_processed','.mat');
+    load(fullfile(saveDir,processedName),'xform_isbrain')
+    xform_isbrain_mice = xform_isbrain_mice.*xform_isbrain;
+end
+mask = AtlasSeeds.*xform_isbrain_mice;
+mask(isnan(mask)) = 0;
 
-%% Correlation Coefficient
-r_HRF_mice_awake_exclude = r_HRF_mice_awake_exclude';
-r_MRF_mice_awake_exclude = r_MRF_mice_awake_exclude';
-r_HRF_mice_anes_exclude = r_HRF_mice_anes_exclude';
-r_MRF_mice_anes_exclude = r_MRF_mice_anes_exclude';
-
-% mean
-r_HRF_mice_awake_exclude_mean = nanmean(r_HRF_mice_awake_exclude,2);
-r_MRF_mice_awake_exclude_mean = nanmean(r_MRF_mice_awake_exclude,2);
-
-r_HRF_mice_anes_exclude_mean  = nanmean(r_HRF_mice_anes_exclude,2);
-r_MRF_mice_anes_exclude_mean  = nanmean(r_MRF_mice_anes_exclude,2);
-
-% sort
-[r_HRF_mice_awake_exclude_mean_sort,I_r_awake] = sort(r_HRF_mice_awake_exclude_mean);
-r_MRF_mice_awake_exclude_mean_sort = r_MRF_mice_awake_exclude_mean(I_r_awake);
-label_region_r_awake = label_region(I_r_awake);
-r_r_awake_mean = corr(r_HRF_mice_awake_exclude_mean_sort,r_MRF_mice_awake_exclude_mean_sort);
-
-[r_HRF_mice_anes_exclude_mean_sort,I_r_anes] = sort(r_HRF_mice_anes_exclude_mean);
-r_MRF_mice_anes_exclude_mean_sort = r_MRF_mice_anes_exclude_mean(I_r_anes);
-label_region_r_anes = label_region(I_r_anes);
-r_r_anes_mean = corr(r_HRF_mice_anes_exclude_mean_sort,r_MRF_mice_anes_exclude_mean_sort);
-
-figure('units','normalized','outerposition',[0 0 1 1])
-subplot(211)
-yyaxis left
-scatter(1:44,r_HRF_mice_awake_exclude_mean_sort,'filled')
-ylabel('NVC r')
-hold on
-yyaxis right
-scatter(1:44,r_MRF_mice_awake_exclude_mean_sort,'filled','d')
-ylabel('NMC r')
-xlabel('Region')
-xticks([1:44])
-xtickangle(0)
-xticklabels(num2str(I_r_awake))
-a = get(gca,'XTickLabel');
-set(gca,'XTickLabel',a,'FontName','Times','fontsize',18)
-title(['Awake, r = ',num2str(r_r_awake_mean)])
-
-subplot(212)
-yyaxis left
-scatter(1:44,r_HRF_mice_anes_exclude_mean_sort,'filled')
-ylabel('NVC r')
-hold on
-yyaxis right
-scatter(1:44,r_MRF_mice_anes_exclude_mean_sort,'filled','d')
-ylabel('NMC r')
-xlabel('Region')
-xticks([1:44])
-xtickangle(0)
-xticklabels(num2str(I_r_anes))
-a = get(gca,'XTickLabel');
-set(gca,'XTickLabel',a,'FontName','Times','fontsize',18)
-title(['Anesthetized, r = ',num2str(r_r_anes_mean)])
-
-sgtitle('Correlation Coefficient Correlation between NVC and NMC')
+mask_exclude = nan(128,128);
+ii = 1; 
+for jj = [3,4,6:25,28,29,31:50]
+   mask_exclude(mask == jj)= ii;
+    ii = ii+1;
+end
